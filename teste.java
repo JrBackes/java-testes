181b56b50b7b4b802cbcf94de2fb75dbd6351855
Junior Backes <junior.backes@hotmail.com.br>
2023-03-18 19:00:27 -0300

    Alteração para adição de classe de teste

--- teste.java
+++ teste.java
@@ -1,42 +1,8 @@
 public class teste{
+    // Classe utilizada para testes do projeto
     
     public static void main(String[] args) {
-        /*Uma fábrica controla o tempo de trabalho sem acidentes pela quantidade de dias. 
-        Faça um programa para converter este tempo em anos, meses e dias, mostrando essa informação. 
-        Assuma que cada mês possui sempre 30 dias. */
-
-
-        /* Lógica
-         * 1 - 
-         * 
-         */
-        int valor = 400;
-
-        // Iniciando variáveis
-        int anos = 0;
-        int meses = 0;
-        int dias = valor;
-
-    
-        if (dias >= 30){
-            for(int i=0;dias>=30;i++){
-            meses++;
-            dias=dias-30;
-            }
-        }
-
-        if (meses >= 12){
-            for(int i=0;meses >= 12;i++){
-                anos++;
-                meses= meses-12;
-            }
-        }
-        System.out.println("");
-        System.out.println("Lembrando que neste exercicio, o mês é considerado com 30 dias, portanto o ano tem 360 dias.");
-        System.out.println("O Valor de dias informado foi " + valor + " dias");
-        System.out.println("Sem acidentes no periodo de " + anos + " anos, " + meses + " meses, " + dias + " dias.");
-      
-
+       
  
    }
     
